5de5d425661a3216d49192d5bf8e702a5b01a652
Chris Lees <chris.lees@bestexhub.com>
2020-05-09 11:39:53 +0100

    Pretty print output

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express    = require('express'),        // Node express (web server)
 	upload     = multer({
 					dest: 'uploads/',
 					limits: {
-						fileSize: 10485760		// 3Mb max upload size
+						fileSize: 10485760		// 10Mb max upload size
 					}
 				}),
 	Logger 		= require("./lib/logger"),	    // Bunyan logging
@@ -25,7 +25,7 @@ function readConfiguration(configFile) {
     try {
         return JSON.parse(content);
     } catch (e) {
-        console.error("Exception parsing the config file, Exception: ", e);
+        console.log("Exception parsing the config file, Exception: ", e);
         throw new Error(e);
     }
 }
@@ -45,7 +45,7 @@ function compileSchema(schemaFile, version) {
 	try {
 		schema = JSON.parse(schema);
 	} catch (e) {
-		console.error("Exception parsing schema file " + schemaFile + " Exception: ", e);
+		console.log("Exception parsing schema file " + schemaFile + " Exception: ", e);
 	   	throw new Error(e);
 	}
 	ajv.addSchema(schema, 'finspec-' + version);
@@ -55,9 +55,9 @@ function compileSchema(schemaFile, version) {
 // compileSchema('schemas/0.1.json', '0.1');
 // compileSchema('schemas/0.2.json', '0.2');
 // compileSchema('schemas/0.3.json', '0.3');
-compileSchema('schemas/1.0.json', '1.0');
-compileSchema('schemas/1.1.json', '1.1');
-compileSchema('schemas/1.2.json', '1.2');
+// compileSchema('schemas/1.0.json', '1.0');
+// compileSchema('schemas/1.1.json', '1.1');
+// compileSchema('schemas/1.2.json', '1.2');
 compileSchema('schemas/2.0.json', '2.0');
 compileSchema('schemas/2.1.json', '2.1');
 
@@ -121,7 +121,7 @@ app.post('/validate', upload.single('json'), function (req, res, next) {
 		log.warn("Request missing FinSpec schema version.");
 		return;
 	}
-	else if (["1.0","1.1","1.2","2.0","2.1"].indexOf(version) == -1) {
+	else if (["2.0","2.1"].indexOf(version) == -1) {
 		res.writeHead(413, {'Content-Type': 'text/plain'});
 		res.end("Request has invalid FinSpec schema version: " + version);
 		req.connection.destroy();
@@ -143,15 +143,20 @@ app.post('/validate', upload.single('json'), function (req, res, next) {
 
 		// Try validating the doc against the schema
 		var valid = ajv.validate('finspec-' + version, data);
+		var message;
 		if (!valid) {
 			var error = ajv.errorsText();
-			res.json({ pass: false, message: error });
+			message = { pass: false, message: error };
 			log.warn("File invalid FinSpec", error);
-			return;
 		}
 
-		res.json({ pass: true, message: 'Good job!' });
-		log.info("File valid FinSpec");
+		else {
+			log.info("File valid FinSpec");
+			message = { pass: true, message: 'Good job!' };
+		}
+
+		res.setHeader('Content-Type', 'application/json');
+		res.end(JSON.stringify(message, null, 3) + "\n");
   	});
 });
 
@@ -161,4 +166,9 @@ app.post('/validate', upload.single('json'), function (req, res, next) {
 var port = configs.port || 9999;
 log.info("Starting validator on port " + port);
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log("\n-----");
+console.log('FinSpec validator is running! The magic happens on port ' + port);
+console.log('To use it, CURL in your file using syntax like this:');
+console.log('curl -XPOST localhost:9999/validate?version=<schema_version> -F json=@/path/to/finspec.json');
+console.log('Please report issues or feedback to happytohelp@fixspec.com');
+console.log("-----\n");
